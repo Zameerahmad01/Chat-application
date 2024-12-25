ef564efdaf4f92d97bef1b49cd631b9274fb58b4
Zameerahmad01 <zameerahmad4877@gmail.com>
2024-12-25 10:22:44 +0500

    Add project model and update user model
    - Introduced a new Project model with a schema that includes name and users fields.
    - Updated User model to hide the password field from query results by setting select to false.

---
+++ backend/src/models/project.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const projectSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      unique: true,
+    },
+    users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  },
+  { timestamps: true }
+);
+
+const Project = mongoose.model("Project", projectSchema);
+
+export default Project;
