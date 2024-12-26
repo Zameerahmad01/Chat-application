ec75c937f0d219675f107f732f4e5c1cb6aeb470
Zameerahmad01 <zameerahmad4877@gmail.com>
2024-12-26 22:42:14 +0500

    Update project routes: Rename 'get' to 'get-all', add user management and project retrieval endpoints

--- backend/src/routes/project.routes.js
+++ backend/src/routes/project.routes.js
@@ -2,12 +2,16 @@ import { Router } from "express";
 import {
   createProject,
   getAllProjects,
+  addUserToProject,
+  getProject,
 } from "../controllers/project.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/create").post(verifyJWT, createProject);
-router.route("/get").get(verifyJWT, getAllProjects);
+router.route("/get-all").get(verifyJWT, getAllProjects);
+router.route("/add-user").put(verifyJWT, addUserToProject);
+router.route("/get-project/:projectId").get(verifyJWT, getProject);
 
 export default router;
