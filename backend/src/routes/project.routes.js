a0d8547292de732e28950d54654da9aad2bfc097
Zameerahmad01 <zameerahmad4877@gmail.com>
2025-01-01 22:12:51 +0500

    Add updateFileTree route to project API for file tree management

--- backend/src/routes/project.routes.js
+++ backend/src/routes/project.routes.js
@@ -4,6 +4,7 @@ import {
   getAllProjects,
   addUserToProject,
   getProject,
+  updateFileTree,
 } from "../controllers/project.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -13,5 +14,6 @@ router.route("/create").post(verifyJWT, createProject);
 router.route("/get-all").get(verifyJWT, getAllProjects);
 router.route("/add-user").put(verifyJWT, addUserToProject);
 router.route("/get-project/:projectId").get(verifyJWT, getProject);
+router.route("/update-filetree").put(verifyJWT, updateFileTree);
 
 export default router;
