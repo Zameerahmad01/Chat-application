634ac4fb9141c862b52f64a1f7c4708a6059ca3c
Zameerahmad01 <zameerahmad4877@gmail.com>
2025-01-01 22:12:22 +0500

    Add fileTree property to project schema for enhanced project structure management

--- backend/src/models/project.model.js
+++ backend/src/models/project.model.js
@@ -10,6 +10,10 @@ const projectSchema = new mongoose.Schema(
       unique: true,
     },
     users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    fileTree: {
+      type: Object,
+      default: {},
+    },
   },
   { timestamps: true }
 );
